edf9cf57fc6bf5cfd769ae6e24620b4cbaa8c879
lokendra22844 <lokendratiwari22844@gmail.com>
2024-05-14 17:15:55 +0530

    Added notification ticket model

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,4 +5,6 @@ dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT,
+    EMAIL_ID:process.env.EMAIL_ID,
+    EMAIL_PASS:process.env.EMAIL_PASS,
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT} =require('./config/serverConfig');
+// const { sendBasicEmail }=require('./services/email-service');
+
+const cron = require('node-cron');
+
 const  setupAndStartServer = ()=>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT, ()=>{
         console.log(` Server started on port ${PORT}`);
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'lokendratiwari22844@gmail.com',
+        //     'This is a testing email.SENDING YOU FROM EMAIL SERVICE DIRECTLY FROM MY SERVER',
+        //     '40,000 RS  DEBITED FROM YOUR ACCOUNT. WE HOPE U LIKE....IT💀.'
+        // );
+        cron.schedule('*/2 * * * *',()=>{
+            console.log('running a task every 2 minutes');
+        });
     }); 
     return app;  
 }
